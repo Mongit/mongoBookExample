9e735e12c4c76781f8369a208146eeb3f96a9ea0
Mon se <monserrat.jimenez2@gmail.com>
2015-09-17 07:58:12 -0500

    Updating refs and getOne Story

--- web/routes/complexSchemas.js
+++ web/routes/complexSchemas.js
@@ -5,7 +5,7 @@ var personFactory = require('./../schemas/ch10/person');
 var storyFactory = require('./../schemas/ch10/story'); 
 
 
-router.get('/showPerson', function(req, res, next) {
+router.get('/getAllPersons', function(req, res, next) {
     var Person = mongoose.model('Person');
     
     Person.find(function (err, persons) {
@@ -14,8 +14,17 @@ router.get('/showPerson', function(req, res, next) {
     });
     
 });
+router.get('/getAllStories', function(req, res, next) {
+    var Story = mongoose.model('Story');
+    
+    Story.find(function (err, stories) {
+        if (err) return next(err);
+        console.log(stories);
+    });
+    
+});
 
-router.get('/population', function(req, res, next) {
+router.get('/populatieMethod', function(req, res, next) {
     //$curl http://localhost:3000/api/complexSchemas/population
     var Story = mongoose.model('Story');
 //You build a query, use the populate command, and then exec() when ready. This will populate with the entire user object.     
@@ -31,7 +40,7 @@ router.get('/population', function(req, res, next) {
     
 });
 
-router.get('/population2', function(req, res, next) {
+router.get('/getSpecificPaths', function(req, res, next) {
     //$curl http://localhost:3000/api/complexSchemas/population
     var Story = mongoose.model('Story');
 //we can specify the individual paths that we require. In this case: name and age
@@ -47,7 +56,7 @@ router.get('/population2', function(req, res, next) {
     
 });
 
-router.post('/person', function(req, res, next) {   
+router.post('/nuevaPerson', function(req, res, next) {   
     var aaron = personFactory({ name: req.body.name, age: req.body.age });
         
     aaron.save(function (err, person) {
@@ -58,13 +67,13 @@ router.post('/person', function(req, res, next) {
     });
 });
 
-router.post('/story/:id', function(req, res, next) {
+router.post('/creaGuardaStory/:id', function(req, res, next) {
     //curl -i -H "Content-Type: application/json" -d '{"title": "Juana title"}' http://localhost:3000/api/complexSchemas/story/55f6ec335d16fcf30391a9d6
     var Person = mongoose.model('Person');
     var id = req.params.id;
     
     Person.findById(id, function (err, found) {
-        if (err) return console.log(err);
+        if (err) return console.log('OH DEAR... ' + err);
         
         var story1 = storyFactory({ 
             title: req.body.title, 
@@ -124,7 +133,7 @@ router.get('/refsToChildren', function(req, res, next) {
     });
 });
 
-router.get('/findStories/:id', function(req, res, next) {
+router.get('/findStories/:id', function(req, res, next) {//person id
     //we could skip populating and directly find() the stories we are interested in.
     var Story = mongoose.model('Story');
     
@@ -133,7 +142,62 @@ router.get('/findStories/:id', function(req, res, next) {
     .exec(function(err, stories) {
         if(err) return console.log(err);
         console.log('The stories are an array: ', stories);
-    })
+        //var thisStory = stories.id(55f6ec335d16fcf30391a9d6);
+        //console.log(thisStory);
+    });
+});
+
+router.get('/updatingRefs/:id', function(req, res, next) {    
+    //If we realized that the _creator of our story was incorrect we updated like this
+    var Story = mongoose.model('Story');
+    var story = undefined;
+    
+    Story
+    .find({ _creator: req.params.id })
+    .exec(function(err, stories) {
+        if(err) return console.log(err);
+        story = stories[0];
+        console.log('The story: ', story);
+   
+        var guille = personFactory({ name: 'Guillermo', age: 19 });
+        guille.save(function (err) {
+            if(err) return console.log(err);
+
+            story._creator = guille;
+            console.log("The new creator " + story._creator.name);
+
+            story.save(function(err) {
+                if(err) return console.log(err);
+
+                Story
+                .findOne({title: /juana title/i })
+                .populate({ path: '_creator', select: 'name' })
+                .exec(function (err, story) {
+                    if(err) return console.log(err);
+
+                    console.log('The creator is %s', story._creator.name);
+                })
+            });
+        });
+        
+    });
+});
+
+
+router.get('/getOne/:id', function(req, res, next) {//id de la story
+//curl http://localhost:3000/api/complexSchemas/getOne/55f6ecda24e5e61c0548a9fc
+
+    var Story = mongoose.model('Story');
+
+    Story
+    .findOne({ _id: req.params.id })
+    .populate('_creator')
+    .exec(function (err, story) {
+        if (err) return console.log("Oh you poor thing" + err);
+        console.log(story);
+        console.log('The creator is %s', story);  
+    });
+    
 });
 
 module.exports = router;
